1fc0504da9012d78b79e2865519ee26869c15b77
k32 <10274441+k32@users.noreply.github.com>
2019-03-05 08:39:04 +0100

    Add benchmark
    Fixes #4

---
+++ benchmark/lib/benchmark.ex
@@ -0,0 +1,7 @@
+list = Enum.to_list(1..10_000)
+map_fun = fn(i) -> [i, i * i] end
+
+Benchee.run(%{
+    "flat_map"    => fn -> Enum.flat_map(list, map_fun) end,
+    "map.flatten" => fn -> list |> Enum.map(map_fun) |> List.flatten end
+})
---
+++ benchmark/mix.exs
@@ -0,0 +1,26 @@
+defmodule Benchmark.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :benchmark,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [{:benchee, "~> 0.13", only: :dev},
+     {:map_sets, "~> 0.1.1", only: :dev}
+    ]
+  end
+end
