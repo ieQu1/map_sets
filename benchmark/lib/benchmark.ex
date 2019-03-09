13f0cf662ac153c5903709e3dad48d178e5f32d6
k32 <10274441+k32@users.noreply.github.com>
2019-03-09 19:22:36 +0100

    Include ordsets and gb_sets in the benchmarks
    Fixes #7

--- benchmark/lib/benchmark.ex
+++ benchmark/lib/benchmark.ex
@@ -1,37 +1,128 @@
 # I don't know Elixir, so the below code probably doesn't make sense
 
-l1 = Enum.to_list(1..10_000)
-l2 = Enum.to_list(10_001..20_000)
-l3 = Enum.to_list(5_000..15_000)
-
-Benchee.run(%{ 'sets from_list' => fn -> :sets.from_list(l1) end,
-               'map_sets from_list' => fn -> :map_sets.from_list(l1) end
-             },
-             time: 10,
-             memory_time: 2,
-             formatters: [ Benchee.Formatters.Console,
-                           {Benchee.Formatters.CSV, file: 'from_list.csv'}
-                         ]
-           )
-
 defmodule MapSets do
-  def run(name, fun, sets0) do
-    sets = Enum.map sets0, &:sets.from_list/1
-    map_sets = Enum.map sets0, &:map_sets.from_list/1
-    Benchee.run(%{ 'sets ' ++ name => fn -> :erlang.apply(:sets, fun, sets) end,
-                   'map_sets ' ++ name => fn -> :erlang.apply(:map_sets, fun, map_sets) end
+  def make_random_list(n, m) do
+    Enum.shuffle (for i <- 1..n, j <- 1..m, do: i)
+  end
+
+  def make_inputs(n) do
+    { Enum.shuffle(Enum.to_list(1 .. n)),
+      Enum.shuffle(Enum.to_list(div(n, 2) .. div(n*3, 2))),
+      Enum.shuffle(Enum.to_list(n + 1 .. 2*n + 1))
+    }
+  end
+
+  def make_sets({a, b, c}) do
+    { :sets.from_list(a),
+      :sets.from_list(b),
+      :sets.from_list(c)
+    }
+  end
+
+  def make_map_sets({a, b, c}) do
+    { :map_sets.from_list(a),
+      :map_sets.from_list(b),
+      :map_sets.from_list(c)
+    }
+  end
+
+  def make_gb_sets({a, b, c}) do
+    { :gb_sets.from_list(a),
+      :gb_sets.from_list(b),
+      :gb_sets.from_list(c)
+    }
+  end
+
+  def make_ordsets({a, b, c}) do
+    { :ordsets.from_list(a),
+      :ordsets.from_list(b),
+      :ordsets.from_list(c)
+    }
+  end
+
+  def inputs do
+    %{
+      "Small (100)" => make_inputs(100),
+      "Middle (100 Thousand)" => make_inputs(100_000),
+      "Big (1 Million)" => make_inputs(1_000_000),
+    }
+  end
+
+  def run(name, fun) do
+    Benchee.run(%{ name ++ ' map_sets' =>
+                   { fn(input) -> fun.(:map_sets, input) end,
+                     before_scenario: &make_map_sets/1
+                   },
+                   name ++ ' sets' =>
+                   { fn(input) -> fun.(:sets, input) end,
+                     before_scenario: &make_sets/1
+                   },
+                   name ++ ' gb_sets' =>
+                   { fn(input) -> fun.(:gb_sets, input) end,
+                     before_scenario: &make_gb_sets/1
+                   },
+                   name ++ ' ordsets' =>
+                   { fn(input) -> fun.(:ordsets, input) end,
+                     before_scenario: &make_ordsets/1
+                   }
                  },
-                 time: 10,
+                 inputs: inputs(),
+                 time: 5,
                  memory_time: 2,
-                 formatters: [ Benchee.Formatters.HTML,
-                               Benchee.Formatters.Console,
-                               {Benchee.Formatters.CSV, file: name ++ '.csv'}
+                 formatters: [ {Benchee.Formatters.HTML, file: name ++ '.html', auto_open: false},
+                               Benchee.Formatters.Console
                              ]
                )
   end
 end
-MapSets.run('empty intersection', :intersection, [l1, l2])
 
-MapSets.run('intersection', :intersection, [l1, l3])
+Benchee.run(%{'from_list sets' => &:sets.from_list/1,
+              'from_list map_sets' => &:map_sets.from_list/1,
+              'from_list gb_sets' => &:gb_sets.from_list/1,
+              'from_list ordsets' => &:ordsets.from_list/1,
+             },
+            inputs: %{ "Small (100)" => MapSets.make_random_list(100, 10),
+                       "Middle (100 Thousand)" => MapSets.make_random_list(100_000, 10),
+                       "Large (1 Million)" => MapSets.make_random_list(1_000_000, 10),
+                     },
+            time: 5,
+            memory_time: 2,
+            formatters: [ {Benchee.Formatters.HTML, file: "from_list.html", auto_open: false},
+                          Benchee.Formatters.Console,
+                        ]
+           )
 
-MapSets.run('union', :union, [l1, l3])
+MapSets.run(
+  'is_element (true)',
+  fn(module, {a, b, c}) -> module.is_element(100, a) end
+)
+
+MapSets.run(
+  'is_element (false)',
+  fn(module, {a, b, c}) -> module.is_element(:a, a) end
+)
+
+MapSets.run(
+  'is_disjoint (true)',
+  fn(module, {a, b, c}) -> module.is_disjoint(a, c) end
+)
+
+MapSets.run(
+  'is_disjoint (false)',
+  fn(module, {a, b, c}) -> module.is_disjoint(a, b) end
+)
+
+MapSets.run(
+  'intersection (disjoint)',
+  fn(module, {a, b, c}) -> module.intersection(a, c) end
+)
+
+MapSets.run(
+  'intersection',
+  fn(module, {a, b, c}) -> module.intersection(a, b) end
+)
+
+MapSets.run(
+  'union',
+  fn(module, {a, b, c}) -> module.union(a, b) end
+)
